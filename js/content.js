624987da72c7bffe2e976b27a371307be75c4055
William Suryawan <suryawan.william@gmail.com>
2019-03-08 16:30:52 +0700

    final

--- js/content.js
+++ js/content.js
@@ -1,3 +1,26 @@
+function isLogin(input) {
+  if(input == true) {
+    $('#content').show()
+    $('#register_form').hide()
+    $('#login').hide()
+    $('#sign_out').show()
+    $('#login_click').hide()
+    $('#register_click').hide()
+    $('#google_signin').hide()
+    $('#body-before-signin').show()
+    $('#container-multimedia').hide()
+  } else {
+    $('#content').hide()
+    $('#register_form').hide()
+    $('#login').hide()
+    $('#sign_out').hide()
+    $('#login_click').show()
+    $('#register_click').show()
+    $('#google_signin').hide()
+    $('#body-before-signin').hide()
+  }
+}
+
 function getBeerOfTheDay () {
   $.ajax({
     url: `${baseURL}/beers/onday`,
@@ -7,9 +30,55 @@ function getBeerOfTheDay () {
     }
   })
     .done(data => {
-      console.log(data.name)
-      getBeerPicture(data.name)
+      $('#beerRow').hide()
+      $('#beerDay').empty()
+      $('#beerDay').show()
+      $('#container-multimedia').show()
+      $.ajax({
+        url: `${baseURL}/pictures?search=${'beer'}`,
+        method: 'GET',
+        data: {
+          token: localStorage.getItem('token')
+        }
+      })
+        .done((picture) => {
+          let picFound = picture.data[random()].assets.preview.url
+          console.log(picFound)
+
+          console.log(data)
+      let beerInformation = `<div class="col-md-6">
+      <img style="height:430px; width:100%" src="${picFound}">
+      </div>
+      <div class="col-md-6">
+      <h5>Name:</h5> ${data.name} <br>
+      <h5>Description:</h5> ${data.style.description} <br>
+      <h5>Category:</h5> ${data.style.name}
+      </div>`
       getYoutube(`Beer ${data.name}`)
+      $('#beerDay').append(beerInformation)
+        })
+        .fail(error => {
+          console.log(error)
+        })
+      
+    })
+    .fail(error => {
+      console.log(error)
+    })
+}
+
+function getBeerList () {
+  $.ajax({
+    url: `${baseURL}/beers/list`,
+    method: 'GET',
+    data: {
+      token: localStorage.getItem('token')
+    }
+  })
+    .done(data => {
+      console.log(data)
+      getBeerPicture(data, data)
+      
     })
     .fail(error => {
       console.log(error)
@@ -20,9 +89,9 @@ function random () {
   return Math.floor(Math.random()*20);
 }
 
-function getBeerPicture(name) {
+function getBeerPicture(name, beers) {
   $.ajax({
-    url: `${baseURL}/pictures?search=${name}`,
+    url: `${baseURL}/pictures?search=${'beer'}`,
     method: 'GET',
     data: {
       token: localStorage.getItem('token')
@@ -31,6 +100,14 @@ function getBeerPicture(name) {
     .done((data) => {
       console.log(data.data[random()].assets.preview.url)
       // return data.data[random()].assets.preview.url
+      let beerlist =''
+      for(let i=0; i<beers.length; i++) {
+        beerlist += `<div class="col-md-3"> 
+        <img style="height:180px; width: 250px" src="${data.data[random()].assets.preview.url}">
+        ${beers[i].name} </div>`
+        
+      }
+      $('#beerRow').append(beerlist)
     })
     .fail(error => {
       console.log(error)
@@ -38,9 +115,19 @@ function getBeerPicture(name) {
 }
 
 $(document).ready(function () {
-  getBeerOfTheDay()
-  let youtubePlayer = `<div id="player"></div>`
-  $('#content').append(youtubePlayer);
+  // let youtubePlayer = ``
+  // $('#content').append(youtubePlayer);
+  if(localStorage.getItem('token')){
+    isLogin(true)
+    
+  }
+  else {
+    isLogin(false)
+  }
+})
+
+function showContent() {
+  event.preventDefault()
   if(localStorage.getItem('token')){
     $('#content').show()
     $('#register_form').hide()
@@ -49,14 +136,10 @@ $(document).ready(function () {
     $('#login_click').hide()
     $('#register_click').hide()
     $('#google_signin').hide()
+    $('#body-before-signin').show()
+    $('#container-multimedia').hide()
+    $('#beerRow').show()
+    $('#beerDay').empty()
+    $('#beerDay').hide()
   }
-  else {
-    $('#content').hide()
-    $('#register_form').show()
-    $('#login').show()
-    $('#sign_out').hide()
-    $('#login_click').show()
-    $('#register_click').show()
-    $('#google_signin').show()
-  }
-})
+}
